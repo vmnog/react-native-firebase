cb6f4dd9825103a8050bfe50db14194b02efca20
Victor Nogueira <victor.nogueira@aitia.com.br>
2021-03-26 15:34:24 -0300

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello Firebase</Text>
+    </View>
+  );
+};
+
+export default App;
